08aac0ad1d5d9615eedb7fa87e6eefa15779d451
Jeremy Dahl <jeremy@palebluedot.ca>
2013-09-04 13:42:45 -0700

    Initial Homework Sync
    Gave the container div a size and position, allowing me to style the
    next/previous & pagination with relative positions.

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,32 @@
 jQuery(document).ready(function($) {
+	
 	// Stuff to do as soon as the DOM is ready;
+ $("#slides").slidesjs({
+    callback: {
+      loaded: function(number) {
+        // Do something awesome!
+        // Passes start slide number
+      },
+      start: function(number) {
+        // Do something awesome!
+        // Passes slide number at start of animation
+      },
+      complete: function(number) {
+        // Do something awesome!
+        // Passes slide number at end of animation
+      $('.slidesjs-pagination-item a').html('<i class="icon-circle-blank"></i>');
+      $('.slidesjs-pagination-item a.active').html('<i class="icon-circle"></i>');
+
+      }
+    }
+  });
+      // find the gallery list items and remove the html from them
+      $('.slidesjs-pagination-item a').html('<i class="icon-circle-blank"></i>');
+      $('.slidesjs-pagination-item a.active').html('<i class="icon-circle"></i>');
+
+      $('.slidesjs-next').html('<i class="icon-chevron-sign-right"></i>');
+      $('.slidesjs-previous').html('<i class="icon-chevron-sign-left"></i>');
+
+
+	// initiate our gallery
 });
